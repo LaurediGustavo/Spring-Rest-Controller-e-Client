02fd153c683aac0776888ed6e3439cdf4ece8100
Lauredi Gustavo <68924576+LaurediGustavo@users.noreply.github.com>
2021-10-22 20:12:39 -0300

    Inicio Projeto

---
+++ src/test/java/com/example/springrestcontrollereclient/SpringRestControllerEClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestcontrollereclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestControllerEClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
